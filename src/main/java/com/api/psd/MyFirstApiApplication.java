4732d918698a6794c1b5f1490fe11f4b15a6b105
dharmalinghamps <165429565+dharmalinghamps@users.noreply.github.com>
2024-03-29 22:06:47 +0000

    Initial Commit

---
+++ src/main/java/com/api/psd/MyFirstApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.psd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstApiApplication.class, args);
+	}
+
+}
